408076630ca9710ef854d27ddd6e6ca82fc8eb0f
Donelkys Santana <donelkys.santana@rubypotion.com>
2019-05-13 21:23:51 -0500

    Initial commit

---
+++ Podfile
@@ -0,0 +1,14 @@
+
+platform :ios, '9.0'
+use_frameworks!
+
+target "UnTaxi" do
+    pod 'Socket.IO-Client-Swift'
+    pod 'Canvas'
+    pod 'AFNetworking'
+    pod 'SwiftyJSON'
+    pod 'MaterialComponents/TextFields'
+    pod 'TextFieldEffects'
+    pod 'Google-Mobile-Ads-SDK'
+    
+end
---
+++ UnTaxi/Constants/Customization.swift
@@ -0,0 +1,22 @@
+//
+//  Customization.swift
+//  UnTaxi
+//
+//  Created by Donelkys Santana on 5/4/19.
+//  Copyright © 2019 Done Santana. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Customization {
+    static var serverData: String!
+    static var appName: String!
+    static var nameShowed: String = "UnTaxi"
+    static var logo: UIImage!
+    static var primaryColor: UIColor = UIColor(red: 250/255, green: 225/255, blue: 84/255, alpha: 1) //#1f1f1f
+    static var textColor: UIColor = UIColor.black
+    static var usaVoucher: Bool = true
+    static var buttonsTitleColor = UIColor.white
+    
+}
