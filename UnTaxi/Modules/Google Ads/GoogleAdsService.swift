e2f68aacd70dfe681cabfc78f837941bd63eda1b
Bocanasa, LLC <info@bocanasa.com>
2023-05-05 08:25:14 -0500

    Added GoogleAdsService file

---
+++ UnTaxi/Modules/Google Ads/GoogleAdsService.swift
@@ -0,0 +1,16 @@
+//
+//  GoogleAdsService.swift
+//  UnTaxi
+//
+//  Created by Donelkys Santana on 3/18/23.
+//  Copyright © 2023 Done Santana. All rights reserved.
+//
+
+import Foundation
+import GoogleMobileAds
+
+class GoogleAdsService {
+	static func getAdsFromGoogle(bannerView: GADBannerView, closure: () -> Void?) {
+		closure()
+	}
+}
